755f52c136689296784e1242fa3843e6a1ed9e01
Victor Miranda <vmm@cin.ufpe.br>
2018-09-21 00:05:49 -0300

    testing recognize event

--- js/game.js
+++ js/game.js
@@ -2,7 +2,23 @@ var score = 0;
 
 function attScore(value) {
     console.log(value)
+    score = value**
     //document.getElementById("score").innerHTML = "Score: " + value;
 }
 
-attScore(10);
+function verifyMarkers() {
+    //var amarker = this.$refs.amarker; // vuejs
+    var nautico = document.querySelector("#nautico")
+    if (nautico.object3D.visible == true) {
+        console.log('marker is visible');
+    }
+    else {
+        console.log('marker is lost');
+    }
+    setTimeout(verifyMarkers, 1000);
+}
+jQuery(document).ready(function() {
+    console.log('ready')
+    verifyMarkers();
+    attScore(10);
+});
